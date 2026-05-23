cf80403f05ab9b71880398eb4f3bc3e807d81ee7
Prometheus Alexander <shs.is.acrux@gmail.com>
2026-05-23 14:47:36 +0300

    Implemented PAUSE

--- game.py
+++ game.py
@@ -184,188 +184,192 @@ class Game:
 
 		if self.player.rect.colliderect(self.boss.rect) and not self.player.invincible_timer > 0:
 			self.player.hit()
-	if c.Event != c.Event.PAUSE:
-		def update(self, dt):
-			if not c.BOSS_TIME:
-				self.boss_timer -= dt * 75
-			self.stars.update(dt)
-			self.asteroids.update(dt)
-			self.all_sprites.update(dt)
-			if not c.BOSS_TIME:
-				self.enemy_spawn_timer += dt
 
-			if not c.BOSS_TIME or self.boss_timer >= 500:
-				if self.enemy_spawn_timer >= self.enemy_spawn_delay:
-					self.enemy_spawn_timer = 0
-					self.enemy_spawn_delay = random.uniform(0.4, 3.6)
-					self.spawn_enemy()
+	def update(self, dt):
+		if not c.BOSS_TIME:
+			self.boss_timer -= dt * 75
+		self.stars.update(dt)
+		self.asteroids.update(dt)
+		self.all_sprites.update(dt)
+		if not c.BOSS_TIME:
+			self.enemy_spawn_timer += dt
 
-			self.asteroid_spawn_timer += dt
+		if not c.BOSS_TIME or self.boss_timer >= 500:
+			if self.enemy_spawn_timer >= self.enemy_spawn_delay:
+				self.enemy_spawn_timer = 0
+				self.enemy_spawn_delay = random.uniform(0.4, 3.6)
+				self.spawn_enemy()
 
-			if self.asteroid_spawn_timer >= self.asteroid_spawn_delay:
-				self.asteroid_spawn_timer = 0
-				self.asteroid_spawn_delay = random.uniform(0.40, 3.2)
-				if len(self.asteroids) < c.level.max_asteroids:
-					self.spawn_asteroid()
+		self.asteroid_spawn_timer += dt
 
-			for enemy in self.enemies:
-				for bullet in self.player_bullets:
-					if enemy.hitbox.colliderect(bullet.rect):
-						bullet.kill()
-						enemy.damage(1)
-						break
+		if self.asteroid_spawn_timer >= self.asteroid_spawn_delay:
+			self.asteroid_spawn_timer = 0
+			self.asteroid_spawn_delay = random.uniform(0.40, 3.2)
+			if len(self.asteroids) < c.level.max_asteroids:
+				self.spawn_asteroid()
 
-			for asteroid in self.asteroids:
-				for bullet in self.player_bullets:
-					if asteroid.hitbox.colliderect(bullet.rect):
-						bullet.kill()
-						asteroid.damage(1)
-						break
+		for enemy in self.enemies:
+			for bullet in self.player_bullets:
+				if enemy.hitbox.colliderect(bullet.rect):
+					bullet.kill()
+					enemy.damage(1)
+					break
 
-			for asteroid in self.asteroids:
-				if asteroid.hitbox.colliderect(self.player.rect) and not self.player.invincible_timer > 0:
+		for asteroid in self.asteroids:
+			for bullet in self.player_bullets:
+				if asteroid.hitbox.colliderect(bullet.rect):
+					bullet.kill()
+					asteroid.damage(1)
+					break
+
+		for asteroid in self.asteroids:
+			if asteroid.hitbox.colliderect(self.player.rect) and not self.player.invincible_timer > 0:
+				self.player.hit()
+				break
+			# elif asteroid.hitbox.colliderect(asteroid.rect):
+			# 	asteroid.damage(1)
+			# 	break
+		if self.boss_timer < 1:
+			self.boss_timer = 0
+			self.boss_spawn()
+
+		if self.player.alive and self.player.invincible_timer <= 0:
+			for bullet in self.enemy_bullets:
+				distance = self.player.pos.distance_to(bullet.pos)
+
+				if distance < self.player.hitbox_radius + bullet.radius:
+					bullet.kill()
 					self.player.hit()
 					break
-				# elif asteroid.hitbox.colliderect(asteroid.rect):
-				# 	asteroid.damage(1)
-				# 	break
-			if self.boss_timer < 1:
-				self.boss_timer = 0
-				self.boss_spawn()
 
-			if self.player.alive and self.player.invincible_timer <= 0:
-				for bullet in self.enemy_bullets:
-					distance = self.player.pos.distance_to(bullet.pos)
+		powerup_hits = pg.sprite.spritecollide(
+			self.player,
+			self.powerups,
+			True
+		)
 
-					if distance < self.player.hitbox_radius + bullet.radius:
-						bullet.kill()
-						self.player.hit()
-						break
+		for powerup in powerup_hits:
+			self.player.apply_powerup(powerup.kind)
 
-			powerup_hits = pg.sprite.spritecollide(
-				self.player,
-				self.powerups,
-				True
+		if not self.player.alive:
+			self.game_over = True
+
+		if self.game_over:
+			self.game_over_scale += self.game_over_scale_dir * 0.4 * dt
+			self.text_alpha -= 0.3
+			self.game_over_backdrop_scale += 2.8 * dt
+			if self.game_over_backdrop_scale > 6:
+				self.game_over_backdrop_scale = 6
+			self.game_over_backdrop_alpha -= 50 * dt
+			if self.game_over_backdrop_alpha < 80:
+				self.game_over_backdrop_alpha = 80
+			return
+
+	def draw(self):
+		self.screen.blit(self.background, (0, 0))
+		self.powerups.draw(self.screen)
+		self.stars.draw(self.screen)
+		self.hud.draw(self.screen)
+		self.asteroids.draw(self.screen)
+		self.all_sprites.draw(self.screen)
+
+		if c.Event == c.Event.PAUSE:
+			pausesurface = pg.Surface((c.WIDTH, c.HEIGHT), pg.SRCALPHA)
+			pausesurface.fill((0, 0, 0, 120))
+
+			pausetext = self.game_over_font.render("| |", True, (255, 255, 255))
+			pause_rect = pausetext.get_rect(center=(c.WIDTH // 2, c.HEIGHT // 2))
+
+			pausesurface.blit(pausetext, pause_rect)
+			self.screen.blit(pausesurface, (0, 0))
+
+		pg.display.flip()
+
+		if c.Event == c.Event.NEXTLEVEL:
+			# Screen whitening
+			if self.overlay_timer > 0:
+				self.overlay_timer -= self.dt / 2
+			else:
+				self.overlay_timer = 0
+
+			if self.overlay_timer > 0:
+				self.rotated_text = pg.Surface((400, 100), pg.SRCALPHA)
+				overlay = pg.Surface((c.WIDTH, c.HEIGHT), pg.SRCALPHA)
+				overlay.fill((0, 0, 50, 25))
+				self.screen.blit(overlay, (0, 0))
+				rect_width = c.WIDTH
+				rect_height = 300
+				self.banner = pg.Surface((rect_width, rect_height), pg.SRCALPHA)
+				self.banner.fill((255, 255, 255, int(self.next_level_backdrop_alpha)-self.overlay_timer//2))
+			if self.text_alpha > 1:
+				self.next_level_backdrop_alpha += 0.01
+			elif self.text_alpha < 100:
+				self.next_level_backdrop_alpha -= 1
+
+			banner_rect = self.banner.get_rect(
+				center=(c.WIDTH // 2, c.HEIGHT // 2)
+			)
+			levelup_font = pg.font.SysFont(f'{c.HOME_DIR}/assets/JetBrainsMonoNerdFont-SemiBold.ttf', 72)
+			self.banner.set_alpha(20)
+			self.screen.blit(self.banner, banner_rect)
+
+			self.rotated_text.blit(next_level_text := levelup_font.render("Next Stage", True, (0, 0, 255)), (c.WIDTH // 2, c.HEIGHT // 2))
+
+			clock = pg.time.Clock()
+			self.dt = clock.tick(60) / 1000
+			self.next_level_scale -= self.dt / 2.5
+
+			rect = self.rotated_text.get_rect(
+				center=(self.rotated_text.get_width() // 2, self.rotated_text.get_height() // 2)
+			)
+			pg.draw.rect(self.screen, self.rotated_text.get_bounding_rect(), (255, 0, 0, 255))
+			rotated_text = pg.transform.rotozoom(
+				self.next_level_text,
+				self.next_level_angle,
+				self.next_level_scale
+			)
+			self.text_alpha -= 0.05
+			self.rotated_text.set_alpha(self.text_alpha)
+			self.next_level_angle += 10 * self.dt
+			self.screen.blit(rotated_text, rect)
+		pg.display.flip()
+
+		if self.game_over:
+			# Screen darkening
+			overlay = pg.Surface((c.WIDTH, c.HEIGHT), pg.SRCALPHA)
+			overlay.fill((0, 0, 0, 25))
+			self.screen.blit(overlay, (0, 0))
+
+			# Expanding dark rectangle
+			rect_width = c.WIDTH
+			rect_height = 300
+
+			banner = pg.Surface((rect_width, rect_height), pg.SRCALPHA)
+
+			banner.fill((0, 0, 0, int(self.game_over_backdrop_alpha)))
+			if self.text_alpha > 1:
+				self.game_over_backdrop_alpha += 0.01
+			elif self.text_alpha < 100:
+				self.game_over_backdrop_alpha -= 1
+
+			banner_rect = banner.get_rect(
+				center=(c.WIDTH // 2, c.HEIGHT // 2)
 			)
 
-			for powerup in powerup_hits:
-				self.player.apply_powerup(powerup.kind)
+			self.screen.blit(banner, banner_rect)
 
-			if not self.player.alive:
-				self.game_over = True
+			rotated_text = pg.transform.rotozoom(
+				self.game_over_text,
+				self.game_over_angle,
+				self.game_over_scale
+			)
+			rotated_text.set_alpha(self.text_alpha)
+			rect = rotated_text.get_rect(
+				center=(c.WIDTH // 2, c.HEIGHT // 2)
+			)
 
-			if self.game_over:
-				self.game_over_scale += self.game_over_scale_dir * 0.4 * dt
-				self.text_alpha -= 0.3
-				self.game_over_backdrop_scale += 2.8 * dt
-				if self.game_over_backdrop_scale > 6:
-					self.game_over_backdrop_scale = 6
-				self.game_over_backdrop_alpha -= 50 * dt
-				if self.game_over_backdrop_alpha < 80:
-					self.game_over_backdrop_alpha = 80
-				return
-		if c.Event == c.Event.PAUSE:
-				pausesurface = pg.Surface((c.WIDTH, c.HEIGHT), pg.SRCALPHA, 32)
-				pausesurface.fill((0, 0, 0, 60))
-				pausetext = self.game_over_font.render("| |", True, (255, 255, 255))
-				pausesurface.blit(pausetext, (c.WIDTH // 2, c.HEIGHT // 2))
-				self.screen.blit(pausesurface, (0, 0))
-				mixer.music.fadeout(1000)
-				pg.display.flip()
-		else:
-				def draw(self):
-					self.screen.blit(self.background, (0, 0))
-					self.powerups.draw(self.screen)
-					self.stars.draw(self.screen)
-					self.hud.draw(self.screen)
-					self.asteroids.draw(self.screen)
-					self.all_sprites.draw(self.screen)
-
-					if c.Event == c.Event.NEXTLEVEL:
-						# Screen whitening
-						if self.overlay_timer > 0:
-							self.overlay_timer -= self.dt / 2
-						else:
-							self.overlay_timer = 0
-
-						if self.overlay_timer > 0:
-							self.rotated_text = pg.Surface((400, 100), pg.SRCALPHA)
-							overlay = pg.Surface((c.WIDTH, c.HEIGHT), pg.SRCALPHA)
-							overlay.fill((0, 0, 50, 25))
-							self.screen.blit(overlay, (0, 0))
-							rect_width = c.WIDTH
-							rect_height = 300
-							self.banner = pg.Surface((rect_width, rect_height), pg.SRCALPHA)
-							self.banner.fill((255, 255, 255, int(self.next_level_backdrop_alpha)-self.overlay_timer//2))
-						if self.text_alpha > 1:
-							self.next_level_backdrop_alpha += 0.01
-						elif self.text_alpha < 100:
-							self.next_level_backdrop_alpha -= 1
-
-						banner_rect = self.banner.get_rect(
-							center=(c.WIDTH // 2, c.HEIGHT // 2)
-						)
-						levelup_font = pg.font.SysFont(f'{c.HOME_DIR}/assets/JetBrainsMonoNerdFont-SemiBold.ttf', 72)
-						self.banner.set_alpha(20)
-						self.screen.blit(self.banner, banner_rect)
-
-						self.rotated_text.blit(next_level_text := levelup_font.render("Next Stage", True, (0, 0, 255)), (c.WIDTH // 2, c.HEIGHT // 2))
-
-						clock = pg.time.Clock()
-						self.dt = clock.tick(60) / 1000
-						self.next_level_scale -= self.dt / 2.5
-
-						rect = self.rotated_text.get_rect(
-							center=(self.rotated_text.get_width() // 2, self.rotated_text.get_height() // 2)
-						)
-						pg.draw.rect(self.screen, self.rotated_text.get_bounding_rect(), (255, 0, 0, 255))
-						rotated_text = pg.transform.rotozoom(
-							self.next_level_text,
-							self.next_level_angle,
-							self.next_level_scale
-						)
-						self.text_alpha -= 0.05
-						self.rotated_text.set_alpha(self.text_alpha)
-						self.next_level_angle += 10 * self.dt
-						self.screen.blit(rotated_text, rect)
-					pg.display.flip()
-
-					if self.game_over:
-						# Screen darkening
-						overlay = pg.Surface((c.WIDTH, c.HEIGHT), pg.SRCALPHA)
-						overlay.fill((0, 0, 0, 25))
-						self.screen.blit(overlay, (0, 0))
-
-						# Expanding dark rectangle
-						rect_width = c.WIDTH
-						rect_height = 300
-
-						banner = pg.Surface((rect_width, rect_height), pg.SRCALPHA)
-
-						banner.fill((0, 0, 0, int(self.game_over_backdrop_alpha)))
-						if self.text_alpha > 1:
-							self.game_over_backdrop_alpha += 0.01
-						elif self.text_alpha < 100:
-							self.game_over_backdrop_alpha -= 1
-
-						banner_rect = banner.get_rect(
-							center=(c.WIDTH // 2, c.HEIGHT // 2)
-						)
-
-						self.screen.blit(banner, banner_rect)
-
-						rotated_text = pg.transform.rotozoom(
-							self.game_over_text,
-							self.game_over_angle,
-							self.game_over_scale
-						)
-						rotated_text.set_alpha(self.text_alpha)
-						rect = rotated_text.get_rect(
-							center=(c.WIDTH // 2, c.HEIGHT // 2)
-						)
-
-						self.screen.blit(rotated_text, rect)
-					pg.display.flip()
+			self.screen.blit(rotated_text, rect)
+		pg.display.flip()
 
 	def run(self):
 		while self.running:
@@ -385,7 +389,7 @@ class Game:
 
 			if c.Event != c.Event.PAUSE:
 				self.update(dt)
-				self.draw()
+			self.draw()
 
 
 		scores = HighScore("John", self.score)
