85638bd036bcf2d9f35666bd2d861127b7616f9c
Prometheus Alexander <shs.is.acrux@gmail.com>
2026-06-01 11:45:50 +0300

    added compile-windows.bat

---
+++ compile-windows.bat
@@ -0,0 +1,12 @@
+@echo off
+
+rmdir /s /q build 2>nul
+rmdir /s /q dist 2>nul
+del *.spec 2>nul
+
+uv run python -m PyInstaller ^
+	--clean ^
+	--onedir ^
+	--name DarkVoid2 ^
+	--add-data "assets;assets" ^
+	main.py
